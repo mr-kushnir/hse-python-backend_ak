98f875f220e2abdef79bc1439a182ff3b8f1998c
mr-kushnir <qwer.74@gmail.com>
2024-10-03 12:35:44 +0300

    hw2

--- tests/test_homework_1.py
+++ tests/test_homework_1.py
@@ -1,13 +1,12 @@
 from http import HTTPStatus
-from typing import Any
+from typing import Any, List, Optional
 
 import pytest
-from async_asgi_testclient import TestClient
+import httpx
 
 from lecture_1.hw.math_plain_asgi import app
 
 
-@pytest.mark.xfail()
 @pytest.mark.asyncio
 @pytest.mark.parametrize(
     ("method", "path"),
@@ -19,15 +18,11 @@ from lecture_1.hw.math_plain_asgi import app
     ],
 )
 async def test_not_found(method: str, path: str):
-    async with TestClient(app) as client:
-        response = await client.open(
-            path,
-            method=method,
-        )
+    async with httpx.AsyncClient(app=app, base_url="http://testserver") as client:
+        response = await client.request(method, path)
         assert response.status_code == HTTPStatus.NOT_FOUND
 
 
-@pytest.mark.xfail()
 @pytest.mark.asyncio
 @pytest.mark.parametrize(
     ("query", "status_code"),
@@ -36,22 +31,20 @@ async def test_not_found(method: str, path: str):
         ({"n": "lol"}, HTTPStatus.UNPROCESSABLE_ENTITY),
         ({"x": "kek"}, HTTPStatus.UNPROCESSABLE_ENTITY),
         ({}, HTTPStatus.UNPROCESSABLE_ENTITY),
-        ({"n": -1}, HTTPStatus.BAD_REQUEST),
-        ({"n": 0}, HTTPStatus.OK),
-        ({"n": 1}, HTTPStatus.OK),
-        ({"n": 10}, HTTPStatus.OK),
+        ({"n": "-1"}, HTTPStatus.BAD_REQUEST),
+        ({"n": "0"}, HTTPStatus.OK),
+        ({"n": "1"}, HTTPStatus.OK),
+        ({"n": "10"}, HTTPStatus.OK),
     ],
 )
 async def test_factorial(query: dict[str, Any], status_code: int):
-    async with TestClient(app) as client:
+    async with httpx.AsyncClient(app=app, base_url="http://testserver") as client:
         response = await client.get("/factorial", params=query)
-
     assert response.status_code == status_code
     if status_code == HTTPStatus.OK:
         assert "result" in response.json()
 
 
-@pytest.mark.xfail()
 @pytest.mark.asyncio
 @pytest.mark.parametrize(
     ("params", "status_code"),
@@ -64,18 +57,16 @@ async def test_factorial(query: dict[str, Any], status_code: int):
     ],
 )
 async def test_fibonacci(params: str, status_code: int):
-    async with TestClient(app) as client:
-        response = client.get("/fibonacci" + params)
-
+    async with httpx.AsyncClient(app=app, base_url="http://testserver") as client:
+        response = await client.get("/fibonacci" + params)
     assert response.status_code == status_code
     if status_code == HTTPStatus.OK:
         assert "result" in response.json()
 
 
-@pytest.mark.xfail()
-@pytest.mark.asyncio()
+@pytest.mark.asyncio
 @pytest.mark.parametrize(
-    ("json", "status_code"),
+    ("json_body", "status_code"),
     [
         (None, HTTPStatus.UNPROCESSABLE_ENTITY),
         ([], HTTPStatus.BAD_REQUEST),
@@ -84,10 +75,9 @@ async def test_fibonacci(params: str, status_code: int):
         ([1.0, 2.0, 3.0], HTTPStatus.OK),
     ],
 )
-async def test_mean(json: dict[str, Any] | None, status_code: int):
-    async with TestClient(app) as client:
-        response = await client.get("/mean", json=json)
-
+async def test_mean(json_body: Optional[List[Any]], status_code: int):
+    async with httpx.AsyncClient(app=app, base_url="http://testserver") as client:
+        response = await client.post("/mean", json=json_body)
     assert response.status_code == status_code
     if status_code == HTTPStatus.OK:
         assert "result" in response.json()
